5f48ccb21a901651b57ad2f39f11e388bfcf2a69
jcgellido <112681480+jcgellido@users.noreply.github.com>
2024-01-24 22:42:27 +0800

    Update main.java

--- main.java
+++ main.java
@@ -15,21 +15,28 @@ public class Q2AA {
 
         LocalDateTime jan31 = LocalDateTime.of(2024, 1, 31, 12, 0);
         Student s1 = new Student("Ambher");
-        Student s2 = new Student("Runa");
+        Student s2 = new Student("Ronna");
+        Teacher t1 = new Teacher("Sir Tarcy", new Subject("CS", 1));
         Subject chem = new Subject("Chemistry", 1);
         SchoolReq sr1 = new SchoolReq("Chem Pre-Lab", chem, jan31, 5, 2, true);
         SchoolReq sr2 = new SchoolReq("Chem quiz", chem, jan31, 3, 2, true);
-        System.out.println("Scenario A");
+        SchoolReq sr3 = new SchoolReq("Exercise07", new Subject("CS", 1), jan31, 4, 2, true);
+        System.out.println("Scenario 1");
         s1.addTask(sr1);
         s1.checkProgress(sr1);
         s1.markDone(sr1);
-        System.out.println("Scenario B");
+        System.out.println("Scenario 2");
         s2.addTask(sr2);
         s2.checkProgress(sr2);
         s2.startTask(sr2);
         s2.checkProgress(sr2);
         s2.markDone(sr2);
-        //Scenario 2 implementations are in classes
+        System.out.println("Scenario 3");
+        Section p1 = new Section("A");
+        Section p2 = new Section("B");
+        t1.assignTask(sr3, p1);
+        t1.assignTask(sr3, p2);
+        
         for(Student student: p1.students) {
             student.addTask(sr3);
             System.out.println("You have a new task: " + sr3.getName());
